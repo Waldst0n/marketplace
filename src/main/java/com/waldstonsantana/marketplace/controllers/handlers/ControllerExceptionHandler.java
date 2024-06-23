e6307506af3464f1b537bc46320e03252aee8b80
Waldst0n <waldston.ws@gmail.com>
2024-06-22 21:51:35 -0300

    Bean Validation

--- src/main/java/com/waldstonsantana/marketplace/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/waldstonsantana/marketplace/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.waldstonsantana.marketplace.controllers.handlers;
 
 import com.waldstonsantana.marketplace.dtos.CustomError;
+import com.waldstonsantana.marketplace.dtos.ValidationError;
 import com.waldstonsantana.marketplace.services.exceptions.DataBaseException;
 import com.waldstonsantana.marketplace.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,6 +30,18 @@ public class ControllerExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+
+        }
         return ResponseEntity.status(status).body(err);
     }
 
